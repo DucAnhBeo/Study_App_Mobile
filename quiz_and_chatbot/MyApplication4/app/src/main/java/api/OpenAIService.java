a7f462c2766d60cadeb3f7257a4bcbf05f1ef3ae
Nguyễn Đức Anh <112877906+DucAnhBeo@users.noreply.github.com>
2025-10-09 11:33:18 +0700

    delete

--- quiz_and_chatbot/MyApplication4/app/src/main/java/api/OpenAIService.java
+++
@@ -1,8 +0,0 @@
-//package api;
-//
-//import com.example.myapplication.BuildConfig;
-//
-//public class OpenAIService {
-//    private static final String API_KEY = BuildConfig.OPENAI_API_KEY;
-//
-//}
